f9bad6cf0c02f22eb949990805acca606163bd33
Oliverac28 <55126545+Oliverac28@users.noreply.github.com>
2019-12-30 19:10:14 +0400

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,109 @@
+
+/*Nav Bar Collapse*/
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("topnav").style.top = "0";
+  } else {
+	setTimeout(4000);
+    document.getElementById("topnav").style.top = "-100px";
+  }
+	setTimeout(10000);
+  prevScrollpos = currentScrollPos;
+}
+
+/*Text Change*/
+
+ var delayInMilliseconds = 5000; //5 seconds
+ var text = ["Apprendre le Français","Rencontrer une communauté","J'aime les thons farcis"];
+ var x;
+ x = 0;
+
+ //while( x < 3) {
+ 	setTimeout (function() {
+  		
+ 		document.getElementById("herotext").innerHTML = text[1];
+  
+ 	}, delayInMilliseconds);
+	
+ 	setTimeout (function() {
+  		
+ 		document.getElementById("herotext").innerHTML = text[2];
+  
+ 	}, delayInMilliseconds);
+
+ 	setTimeout (function() {
+  		
+ 		document.getElementById("herotext").innerHTML = text[3];
+  
+ 	}, delayInMilliseconds);
+  x = x + 1;
+	
+  if (x === 2){
+ 		x = 0;
+ 	}
+ } 
+
+// Modal
+
+function openImage(imgs) {
+	console.log("click");
+  var expandImg = document.getElementById("expandedImg");
+  var modal = document.getElementById("modal_id");
+  expandImg.src = imgs.src;
+  modal.style.display = "block";
+	returnImage(imgs.src);
+}
+
+function returnImage(x){
+	return x;
+}
+	
+
+
+
+//function clickImage(){
+//	var imageClicked = document.getElementsByTagName();
+//}
+// 
+//function getImage(){
+//	var imageActive = document.getElementsByTagName(clickImage());
+//	var modalClass = document.getElementById('modal_id');
+//	
+//	return imageActive;
+//	
+//	console.log("imageActive")
+//}
+//
+//function setImage(){
+//	
+//}
+
+//function displayModal(){
+//	var modalClass = document.getElementById('modal_id');	
+//	modalClass.style.display = "block";
+//	console.log(modalClass);
+//}
+//
+//
+//
+function closeModal(){
+	var modalClass = document.getElementById(modal_id);	
+	modalClass.style.display = "none";
+	console.log(modalClass);
+	
+}
+
+function close(){
+	document.getElementById("intro").intro.style.display = "none";
+	}
+
+function errorFunction(){
+	console.log("error");
+}
+ 
+
+ 
+
---
+++ js/script2.js
@@ -0,0 +1,92 @@
+/*Nav Bar Collapse*/
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("topnav").style.top = "0";
+  } else {
+	setTimeout(4000);
+    document.getElementById("topnav").style.top = "-100px";
+  }
+	setTimeout(10000);
+  prevScrollpos = currentScrollPos;
+}
+
+//End Nav Bar Collapse
+
+
+
+function closeBtn(){
+  console.log("Hello");
+  document.getElementById("intro").style.display = "none";
+  
+}
+
+/*About Slideshow*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  
+  // slides[slideIndex-1].style.display = "block";
+ 
+} 
+
+//End about slideshow
+
+/*Modal Menu*/
+
+function openMenu(){
+  var menu = document.getElementById('menuModal');
+
+  if (menu.style.display === "none"){
+    menu.style.display = "block";
+  }
+  else{
+    menu.style.display = "none";
+  }
+}
+
+function textChange(){
+  var txt1 = document.getElementById("txt1");
+  var txt2 = "a community"
+  var txt3 = "Benefits";
+  var delayInMilliseconds = 1000; 
+
+  console.log("textChange");
+  setTimeout (function() {
+    console.log("CLick");
+  	
+    txt1 = txt2;
+
+    setTimeout (function() {
+  		
+      txt2 = txt3;
+   
+    }, delayInMilliseconds);
+ 
+  }, delayInMilliseconds);
+
+  
+ 
+}
